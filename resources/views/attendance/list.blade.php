6e5f5fd280aaacd2369740d5e47dcd22375c5563
gidj02 <ginodejesus02@gmail.com>
2015-06-24 02:02:55 +0800

    [TASK] added search filter

--- resources/views/attendance/list.blade.php
+++ resources/views/attendance/list.blade.php
@@ -4,9 +4,18 @@
 <div ng-controller="list-controller">
 	<div class="panel panel-primary">
 		<!-- Default panel contents -->
-		<div class="panel-heading">Attendance</div>
+		<div class="panel-heading">
+			<div class="row">
+				<div class="col-md-6"><h4>Attendance</h4></div>
+				<div class="col-md-6">
+					<div class="form-group-sm">
+						<input type="text" class="form-control" placeholder="Search Member" ng-model="filterName">
+					</div>
+				</div>
+			</div>		
+		</div>
 		<div class="panel-body">
-			<p>This is the listing of Jesus the River of Life Gospel Church's members</p>
+			<p>This is the listing of Jesus the River of Life Gospel Church's members</p>	
 		</div>
 
 		<!-- Table -->
@@ -17,7 +26,7 @@
 				<th>Last Name</th>
 				<th>Date Attended</th>
 			</tr>
-			<tr ng-repeat="(key, member) in members">
+			<tr ng-repeat="(key, member) in members | filter: filterName">
 				<td>@{{ member.first_name }}</td>
 				<td>@{{ member.middle_name }}</td>
 				<td>@{{ member.last_name }}</td>
@@ -25,16 +34,16 @@
 			</tr>
 			<tr>
 				<td>
-					<div class="input-group"><input type="text" class="form-control" name="first_name" placeholder="First Name" ng-model="member.firstname" required></div>
+					<div class="form-group-sm"><input type="text" class="form-control" name="first_name" placeholder="First Name" ng-model="member.firstname" required></div>
 				</td>
 				<td>
-					<div class="input-group"><input type="text" class="form-control" name="middle_name" placeholder="Middle Name" ng-model="member.middlename" required></div>
+					<div class="form-group-sm"><input type="text" class="form-control" name="middle_name" placeholder="Middle Name" ng-model="member.middlename" required></div>
 				</td>
 				<td>
-					<div class="input-group"><input type="text" class="form-control" name="last_name" placeholder="Last Name" ng-model="member.lastname" required></div>
+					<div class="form-group-sm"><input type="text" class="form-control" name="last_name" placeholder="Last Name" ng-model="member.lastname" required></div>
 				</td>
 				<td>
-					<div class="input-group"><input type="date" class="form-control" name="first_attend" value="" ng-model="member.firstattend" required></div>
+					<div class="form-group-sm"><input type="date" class="form-control" name="first_attend" value="" ng-model="member.firstattend" required></div>
 				</td>
 			</tr>
 		</table>
